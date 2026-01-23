932198d8cfb127d7e575d82b4abd4260bbf2049a
Nauman <inaumanmajeed@gmail.com>
2026-01-23 16:04:17 +0500

    Add preconnect links for Google Fonts in layout and optimize MongoDB connection options by removing unnecessary settings

--- app/layout.js
+++ app/layout.js
@@ -10,6 +10,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      </head>
       <body>
         <Providers>{children}</Providers>
       </body>
